bc89339ed37f5cdb9d24aeb81d282292e77a7db4
John Kitchin <jkitchin@cmu.edu>
2013-03-03 11:01:43 -0500

    source-commit

--- _deploy.py
+++ _deploy.py
@@ -1,11 +1,11 @@
 #
 
-import commands, shutil
+import commands, os, shutil
 
-commands.getstatusoutput('rm -fr _deploy/*')
-commands.getstatusoutput('cp -R _site/* _deploy')
+print commands.getstatusoutput('rm -fr _deploy/*')
+print commands.getstatusoutput('cp -R _site/* _deploy')
 os.chdir('_deploy')
-commands.getstatusoutput('git add .')
-commands.getstatusoutput('git add u')
-commands.getstatusoutput('git commit -m "deploy"')
-commands.getstatusoutput('git push origin master')
+print commands.getstatusoutput('git add .')
+print commands.getstatusoutput('git add u')
+print commands.getstatusoutput('git commit -m "deploy"')
+print commands.getstatusoutput('git push origin master')
