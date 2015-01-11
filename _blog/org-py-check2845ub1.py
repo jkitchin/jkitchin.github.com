0933b4ae054572437bd5012cba54c822f4d59f13
John Kitchin <jkitchin@andrew.cmu.edu>
2015-01-11 11:43:52 -0500

    source-commit

---
+++ _blog/org-py-check2845ub1.py
@@ -0,0 +1,13 @@
+from scipy.integrate import odeint
+
+
+def ode(y, x):
+
+
+    return -k * x
+
+xspan = [0, 1]
+y0 = 1
+
+sol = odeint(ode, y0, xspan)
+print sol
---
+++ _blog/org-py-check5840_GI.py
@@ -0,0 +1,25 @@
+class Unit(np.ndarray):
+
+    BASE_UNITS = ['m', 's', 'kg', 'K', 'mol', 'coul', 'candela']
+    
+    def __new__(cls, input_array, exponents=None, label=None):
+        # Input array is an already formed ndarray instance
+        # We first cast to be our class type
+        obj = np.asarray(input_array).view(cls)
+        # add the new attribute to the created instance
+        obj.exponents = np.array(exponents)
+        obj.label = label
+        # Finally, we must return the newly created object:
+        return obj
+
+    def __eq__(self, other):
+        'implement a==b for units'
+        if not isinstance(other, Unit):
+            raise IncompatibleUnits(['you should not compare objects'
+                                     ' with different units.'
+                                     ' {0} == {1}'.format(self, other)])
+
+        return (np.all(self.exponents == other.exponents)
+                and np.all(np.array(self) == np.array(other)))
+
+print np.float64(2) == Unit(2, [1, 0, 0, 0, 0, 0, 0])
