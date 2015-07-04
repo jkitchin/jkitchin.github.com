550ec762bd0acfee43bec57e0761aa8cb70187f3
John Kitchin <jkitchin@andrew.cmu.edu>
2015-07-04 11:50:12 -0400

    source-commit

---
+++ _blog/org2xml.el
@@ -0,0 +1,115 @@
+
+:;exec emacs -batch -l $0 -f main "$@"
+(require 'org)
+(require 'xml)
+
+(defun print-tag (name attrs &optional closingp)
+  "Print an xml tag with symbol NAME and ATTRS (a cons list of (attribute . value)).
+if CLOSINGP print the closing tag instead."
+  (format
+   "<%s%s%s>"
+   (if closingp "/" "")
+   name
+   (if (and attrs (not closingp))
+       (concat
+	" "
+	(mapconcat
+	 (lambda (x)
+	   (format "%s=\"%s\""
+		   (car x)
+		   (cdr x)))
+	 attrs
+	 " "))
+     "")))
+
+(defmacro tag (name attributes &rest body)
+  `(format "%s%s%s"
+	   (print-tag ,name ,attributes nil)
+           (concat
+	   ,@body)
+	   (print-tag ,name nil t)))
+
+(defun main ()
+  (find-file (car command-line-args-left))
+  (princ (tag 'root `((filename . ,(buffer-file-name))
+			  (indexed-on . ,(current-time-string)))
+		  ;; map the headlines
+		  (mapconcat
+		   'identity
+		   (org-map-entries
+		    (lambda ()
+		      (let* ((tags (org-get-tags))
+			     (heading-components (org-heading-components))
+			     (todo (nth 2 heading-components))
+			     (title (nth 4 heading-components))
+			     (thislevel (nth 0 heading-components))
+			     (properties (org-entry-properties))
+			     (elem (org-element-at-point))
+			     (bp (org-element-property :contents-begin elem))
+			     (ep (org-element-property :contents-end elem))
+			     (content (buffer-substring bp ep)))
+			(tag 'heading `((level . ,thislevel))
+			     (tag 'title () (xml-escape-string title))
+			     (tag 'tags () (mapconcat 'identity tags " "))
+			     (when todo
+			       (tag 'todo () todo))
+			     (tag 'properties ()
+				  (mapconcat
+				   (lambda (x)
+				     (tag 'property `((label . ,(car x))) (cdr x)))
+				   properties
+				   ""))
+			     (tag 'content ()
+				  (format "%s" (xml-escape-string content)))))))
+		   "")
+
+		  ;; map specific element types
+		  (tag 'source-blocks ()
+		       (mapconcat
+			'identity
+			(org-element-map
+			    (org-element-parse-buffer)
+			    'src-block
+			  (lambda (element)
+			    (tag 'src-block
+				 `((language .
+					     ,(org-element-property
+					       :language element)))
+				 (tag 'contents ()
+				      (xml-escape-string
+				       (org-element-property :value element)))))) ""))
+
+		  (tag 'tables ()
+		       (mapconcat
+			'identity
+			(org-element-map
+			    (org-element-parse-buffer)
+			    'table
+			  (lambda (element)
+			    (tag 'table ()
+				 (when (org-element-property :caption element)
+				   (tag 'caption ()
+					(format
+					 "%s"
+					 (org-element-property
+					  :caption element))))
+				 (xml-escape-string
+				  (buffer-substring
+				   (org-element-property :contents-begin element)
+				   (org-element-property :contents-end element))))))
+			""))
+
+		  (tag 'paragraphs ()
+		       (mapconcat
+			'identity
+			(org-element-map
+			    (org-element-parse-buffer)
+			    'paragraph
+			  (lambda (element)
+			    (tag 'paragraph ()
+				 (xml-escape-string
+				  (buffer-substring
+				   (org-element-property :contents-begin element)
+				   (org-element-property :contents-end element))))))
+			""
+			)))))
