20c7affcad5bcc196ac98e6ecb7ec2d966995b10
John Kitchin <jkitchin@andrew.cmu.edu>
2017-01-31 09:56:28 -0500

    deployment

--- dept_publications.py
+++ dept_publications.py
@@ -18,7 +18,7 @@ for YEAR in [#'2005',
     # this query includes Neil Donahue
     ss = ScopusSearch('(affil(carnegie mellon university and chemical engineering)'
                       ' or au-id(7004596535)) AND pubyear is {}'
-                      ' AND  ( LIMIT-TO ( DOCTYPE ,  "ar" ) '.format(YEAR),
+                      ' AND  ( LIMIT-TO ( DOCTYPE ,  "ar" )) '.format(YEAR),
                       refresh=True)
 
     with open('dept-publications-{}.html.mako'.format(YEAR), 'w') as f:
